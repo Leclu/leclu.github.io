9c57d23153ba0cdb413cd90118e19f92b9c3d399
Leclu <57877797+Leclu@users.noreply.github.com>
2023-10-24 22:29:06 +0200

    Update tering.js

--- tering.js
+++ tering.js
@@ -1,4 +1,14 @@
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
- }
+function random255() {
+    return Math.round(Math.random() * 255);
+  }
+  
+  function setRandomColor() {
+    let r = random255();
+    let g = random255();
+    let b = random255();
+    let color = `rgb(${r}, ${g}, ${b})`;
+  
+    let element = document.body;
+  
+    element.style.backgroundColor = color;
+}
