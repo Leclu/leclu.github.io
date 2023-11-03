5bce6715a2c78fd84d287a3e01058adb093fb148
Leclu <57877797+Leclu@users.noreply.github.com>
2023-11-03 15:37:20 +0100

    Update tering.js

--- tering.js
+++ tering.js
@@ -1,9 +1,7 @@
 var toggleButton = document.getElementById("toggleButton");
 var hiddenDivision = document.getElementById("hiddenDivision");
 
-// Add a click event listener to the button
 toggleButton.addEventListener("click", function() {
-    // Toggle the visibility of the hidden division
     if (hiddenDivision.style.display === "none") {
         hiddenDivision.style.display = "block";
     } else {
